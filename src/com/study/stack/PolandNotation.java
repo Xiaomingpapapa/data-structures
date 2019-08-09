e0276826d4606c6867844b637bd9f4cd331492dc
33053 <zhiming.chen@1hai.cn>
2019-08-09 09:24:10 +0800

    feat: 新增中缀表达式的字符串分割

--- src/com/study/stack/PolandNotation.java
+++ src/com/study/stack/PolandNotation.java
@@ -7,10 +7,15 @@ import java.util.Stack;
 public class PolandNotation {
     public static void main(String[] args) {
         // 中缀转换成逆波兰表达式 (3 + 5) * 2 - 3 => 3 5 + 2 * 3 -
-        String polandNoationExpression = "30 5 + 2 * 3 -";
-        List<String> charList = getListString(polandNoationExpression);
-        cal(charList);
+//        String polandNoationExpression = "30 5 + 2 * 3 -";
+//        List<String> charList = getListString(polandNoationExpression);
+//        cal(charList);
+        // 中缀表达式字符 list 获取 (3+5)*2-3
+        String infixExpression = "(3+5)*2-3";
+        List list = getInfixStringList(infixExpression);
+        System.out.println(list);
     }
+
 
     private static void cal(List<String> charList) {
         //初始化栈存放操作数
@@ -40,7 +45,32 @@ public class PolandNotation {
         System.out.printf("运算结果=%d", stack.pop());
     }
 
-    private static List<String> getListString(String polandNoationExpression) {
+    public static List<String> getInfixStringList(String expression) {
+        List<String> list = new ArrayList<>();
+        char c = ' ';
+        int index = 0;
+        //拼接多位数
+        String num = "";
+        do {
+            if ((c = expression.charAt(index)) < 48 || (c = expression.charAt(index)) > 57) {
+                //说明 c 不是数字
+                list.add(String.valueOf(c));
+                index++;
+            } else {
+                num = "";
+                //拼接多位数
+                while (index < expression.length() && ((c = expression.charAt(index)) >= 48 && (c = expression.charAt(index)) <= 57)) {
+                    num += String.valueOf(expression.charAt(index));
+                    index++;
+                }
+                list.add(num);
+            }
+
+        } while (index < expression.length());
+        return list;
+    }
+
+    public static List<String> getListString(String polandNoationExpression) {
         List<String> list = new ArrayList<>();
         //根据空格分割字符
         String[] chars = polandNoationExpression.split(" ");
@@ -49,8 +79,6 @@ public class PolandNotation {
         }
         return list;
     }
-
-
 }
 
 
