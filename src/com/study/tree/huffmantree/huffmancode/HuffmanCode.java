c27be2e5dd6fa8bd6edbef3617aee09fcef8ecd2
zhiming <1661251853@qq.com>
2019-09-02 22:32:00 +0800

    新增 huffmanCode 树的创建

---
+++ src/com/study/tree/huffmantree/huffmancode/HuffmanCode.java
@@ -0,0 +1,138 @@
+package com.study.tree.huffmantree.huffmancode;
+
+import java.util.*;
+
+public class HuffmanCode {
+    public static void main(String[] args) {
+        String content = "i like like like java do you like a java";
+        byte[] bytes = content.getBytes();
+        List<Node> list = getNodeList(bytes);
+        System.out.println(list);
+
+        Node root = createHuffmanTree(list);
+        preOrder(root);
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) {
+            System.out.println("赫夫曼树为空");
+            return;
+        }
+        root.preOrder();
+    }
+
+    /**
+     * 构造 huffman 树节点列表
+     * @param bytes
+     * @return
+     */
+    public static List<Node> getNodeList(byte[] bytes) {
+        // 进行字节的权重映射
+        Map<Byte, Integer> weightMap = new HashMap<>();
+        for (byte aByte : bytes) {
+            Integer weight = weightMap.get(aByte);
+            if (weight == null) {
+                weightMap.put(aByte, 1);
+            } else {
+                weightMap.put(aByte, weight + 1);
+            }
+        }
+
+        // 初始化 node
+        List<Node> nodeList = new ArrayList<>();
+        weightMap.forEach((k, v) -> {
+            Node node = new Node(k, v);
+            nodeList.add(node);
+        });
+        return nodeList;
+    }
+
+    /**
+     * 创建 huffman 树
+     * @param nodes
+     * @return
+     */
+    public static Node createHuffmanTree(List<Node> nodes) {
+        while (nodes.size() > 1) {
+            Collections.sort(nodes);
+            Node left = nodes.get(0);
+            Node right = nodes.get(1);
+            Node parent = new Node(null, left.getWeight() + right.getWeight());
+            parent.setLeft(left);
+            parent.setRight(right);
+
+            nodes.remove(left);
+            nodes.remove(right);
+            nodes.add(parent);
+        }
+        return nodes.get(0);
+    }
+}
+
+class Node implements Comparable<Node> {
+    private Byte data;
+    //权重
+    private Integer weight;
+    private Node left;
+    private Node right;
+
+    public Node(Byte data, Integer weight) {
+        this.data = data;
+        this.weight = weight;
+    }
+
+    public void preOrder() {
+        System.out.println(this);
+        if (this.getLeft() != null) {
+            this.getLeft().preOrder();
+        }
+        if (this.getRight() != null) {
+            this.getRight().preOrder();
+        }
+    }
+
+    public Byte getData() {
+        return data;
+    }
+
+    public void setData(Byte data) {
+        this.data = data;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.weight - o.getWeight();
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", weight=" + weight +
+                '}';
+    }
+}
